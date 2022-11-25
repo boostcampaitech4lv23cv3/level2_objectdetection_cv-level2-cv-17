b9ba9e2b167c50b9ea4ff9901405e5b86ea3639d
서장원_T4105 <jangwon.seo@snu.ac.kr>
2022-11-25 04:29:47 +0000

    :bug: Fix the bug on `MMDetWandbHook._add_ground_truth()` [val 데이터 추가 후 발생한 에러 해결](https://www.notion.so/jwseo/val-588adf6365e649c092e85bfb442c3946) 참고하기

--- mmdetection/mmdet/core/hook/wandblogger_hook.py
+++ mmdetection/mmdet/core/hook/wandblogger_hook.py
@@ -376,6 +376,7 @@ class MMDetWandbHook(WandbLoggerHook):
             wandb_boxes = self._get_wandb_bboxes(bboxes, labels)
 
             # Get dict of masks to be logged.
+            masks = None if set(masks) == set([None]) else masks     # 👈 코드 추가
             if masks is not None:
                 wandb_masks = self._get_wandb_masks(
                     masks,
