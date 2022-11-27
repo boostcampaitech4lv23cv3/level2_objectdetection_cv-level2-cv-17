f9c677d649ebab28722c1351d4771649bee2a159
JKbin <wiken0072@naver.com>
2022-11-27 08:42:23 +0000

    :wrench: Add Augmentation+TTA

--- mmdetection/configs/_trash_/_base_/datasets/coco_detection_heavyaug.py
+++ mmdetection/configs/_trash_/_base_/datasets/coco_detection_heavyaug.py
@@ -2,6 +2,9 @@
 dataset_type = "CocoDataset"
 data_root = "/opt/ml/dataset/"
 
+"""
+    Heavy Augmenteation + TTA(RandomFlip)
+"""
 classes = (
     "General trash",
     "Paper",
@@ -14,63 +17,65 @@ classes = (
     "Battery",
     "Clothing",
 )
+
 img_scale = (512, 512)
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True
 )
-
-
-heavy_aug_default = [
-    dict(type="HorizontalFlip", p=0.5),
-    dict(type="VerticalFlip", p=0.5),
+albu_train_transforms = [
     dict(
-        type="GaussNoise",
-        var_limit=(10.0, 50.0),
-        mean=0,
-        per_channel=True,
-        always_apply=False,
+        type="ShiftScaleRotate",
+        shift_limit=0.0625,
+        scale_limit=0.0,
+        rotate_limit=0,
+        interpolation=1,
         p=0.5,
     ),
     dict(
         type="RandomBrightnessContrast",
-        brightness_limit=0.2,
-        contrast_limit=0.2,
-        brightness_by_max=True,
-        always_apply=False,
-        p=0.5,
+        brightness_limit=[0.1, 0.3],
+        contrast_limit=[0.1, 0.3],
+        p=0.2,
     ),
     dict(
-        type="HueSaturationValue",
-        hue_shift_limit=20,
-        sat_shift_limit=30,
-        val_shift_limit=20,
-        p=0.5,
+        type="OneOf",
+        transforms=[
+            dict(
+                type="RGBShift",
+                r_shift_limit=10,
+                g_shift_limit=10,
+                b_shift_limit=10,
+                p=1.0,
+            ),
+            dict(
+                type="HueSaturationValue",
+                hue_shift_limit=20,
+                sat_shift_limit=30,
+                val_shift_limit=20,
+                p=1.0,
+            ),
+        ],
+        p=0.1,
     ),
-    dict(type="RGBShift", r_shift_limit=20, g_shift_limit=20, b_shift_limit=20, p=0.5),
+    dict(type="JpegCompression", quality_lower=85, quality_upper=95, p=0.2),
+    dict(type="ChannelShuffle", p=0.1),
     dict(
-        type="ShiftScaleRotate",
-        shift_limit=0.0625,
-        scale_limit=0.1,
-        rotate_limit=45,
-        interpolation=1,
-        border_mode=4,
-        rotate_method="largest_box",  # or ellipse
-        p=0.5,
+        type="OneOf",
+        transforms=[
+            dict(type="Blur", blur_limit=3, p=1.0),
+            dict(type="MedianBlur", blur_limit=3, p=1.0),
+        ],
+        p=0.1,
     ),
 ]
-
-# horizontalflip, verticalflip, GaussNoise, RandomBrightnessContrast
-# HueSaturationValue, RGBShift, ShiftScaleRotate
-
-
 train_pipeline = [
     dict(type="LoadImageFromFile"),
-    dict(type="LoadAnnotations", with_bbox=True),
-    # dict(type="Resize", img_scale=img_scale, keep_ratio=True),
-    # dict(type="RandomFlip", flip_ratio=0.5),
+    dict(type="LoadAnnotations", with_bbox=True, with_mask=False),
+    dict(type="Resize", img_scale=img_scale, keep_ratio=True),
+    dict(type="Pad", size_divisor=32),
     dict(
         type="Albu",
-        transforms=heavy_aug_default,
+        transforms=albu_train_transforms,
         bbox_params=dict(
             type="BboxParams",
             format="pascal_voc",
@@ -78,16 +83,33 @@ train_pipeline = [
             min_visibility=0.0,
             filter_lost_elements=True,
         ),
-        keymap={"img": "image", "gt_masks": "masks", "gt_bboxes": "bboxes"},
+        keymap={"img": "image", "gt_bboxes": "bboxes"},
         update_pad_shape=False,
         skip_img_without_anno=True,
     ),
+    # dict(
+    #     # multi-scale
+    #     type="Resize",
+    #     img_scale=[(1024, 1024)],
+    #     multiscale_mode="range",
+    #     ratio_range=(0.5, 1.5),
+    #     keep_ratio=True,
+    # ),
     dict(type="Normalize", **img_norm_cfg),
-    dict(type="Pad", size_divisor=32),
     dict(type="DefaultFormatBundle"),
-    dict(type="Collect", keys=["img", "gt_bboxes", "gt_labels"]),
+    dict(
+        type="Collect",
+        keys=["img", "gt_bboxes", "gt_labels"],
+        meta_keys=(
+            "filename",
+            "ori_shape",
+            "img_shape",
+            "img_norm_cfg",
+            "pad_shape",
+            "scale_factor",
+        ),
+    ),
 ]
-
 
 test_pipeline = [
     dict(type="LoadImageFromFile"),
@@ -97,7 +119,9 @@ test_pipeline = [
         flip=False,
         transforms=[
             dict(type="Resize", keep_ratio=True),
-            dict(type="RandomFlip"),
+            # dict(type="RandomFlip"),
+            dict(type="RandomFlip", flip_ratio=0.5, direction="vertical"),
+            dict(type="RandomFlip", flip_ratio=0.5, direction="horizontal"),
             dict(type="Normalize", **img_norm_cfg),
             dict(type="Pad", size_divisor=32),
             dict(type="ImageToTensor", keys=["img"]),
@@ -106,20 +130,21 @@ test_pipeline = [
     ),
 ]
 
+
 data = dict(
     samples_per_gpu=4,
     workers_per_gpu=2,
     train=dict(
         classes=classes,
         type=dataset_type,
-        ann_file=data_root + "train.json",
+        ann_file=data_root + "train_groupk_0.json",
         img_prefix=data_root,
         pipeline=train_pipeline,
     ),
     val=dict(
         classes=classes,
         type=dataset_type,
-        ann_file=data_root + "val.json",
+        ann_file=data_root + "val_groupk_0.json",
         img_prefix=data_root,
         pipeline=test_pipeline,
     ),
@@ -132,4 +157,4 @@ data = dict(
     ),
 )
 
-evaluation = dict(interval=1, metric="bbox", classwise=True)
+evaluation = dict(interval=1, metric="bbox")
