27d4e870d6807b2724bd9f4ca11f0f6b0c134086
서장원_T4105 <jangwon.seo@snu.ac.kr>
2022-12-01 11:24:11 +0900

    :seedling: Set auto_resume, checkpoint_config on `default_runtime.py`

--- mmdetection/configs/_trash_/_base_/default_runtime.py
+++ mmdetection/configs/_trash_/_base_/default_runtime.py
@@ -1,4 +1,9 @@
-checkpoint_config = dict(interval=1)
+checkpoint_config = dict(
+    max_keep_ckpts=2,
+    interval=1,
+    # save_last=True,
+    # by_epoch=False,
+)
 # yapf:disable
 log_config = dict(
     interval=50,
@@ -7,16 +12,17 @@ log_config = dict(
         # dict(type='CheckpointHook'),
         # dict(type='TensorboardLoggerHook')
         # dict(type='MlflowLoggerHook'),
-        dict(type='MMDetWandbHook',
-             init_kwargs={
-                'project': 'object-detection',
-                'entity': 'boostcamp-ai-tech-4-cv-17',
-             },
-             interval=10,
-             log_checkpoint=True,
-             log_checkpoint_metadata=True,
-             num_eval_images=100,
-             bbox_score_thr=0.3
+        dict(
+            type="MMDetWandbHook",
+            init_kwargs={
+                "project": "object-detection",
+                "entity": "boostcamp-ai-tech-4-cv-17",
+            },
+            interval=10,
+            log_checkpoint=True,
+            log_checkpoint_metadata=True,
+            num_eval_images=100,
+            bbox_score_thr=0.3,
         ),
     ],
 )
@@ -27,6 +33,8 @@ dist_params = dict(backend="nccl")
 log_level = "INFO"
 load_from = None
 resume_from = None
+auto_resume = True
+
 workflow = [("train", 1)]
 
 # disable opencv multithreading to avoid system being overloaded
